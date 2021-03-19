3b783ae74cb40386554f5f3dec99cd6595851e4d
Anish Nagariya <anishnagariya07@gmail.com>
2021-03-19 16:21:50 -0400

    Delete backend directory

--- backend/database/models/token.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from ..db import db
-import mongoengine_goodjson as gj
-
-
-class TokenBlockList(gj.Document):
-    jti = db.StringField(required=True)
-    created_at = db.DateTimeField(default=datetime.datetime.now())
