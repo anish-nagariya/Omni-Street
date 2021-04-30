7819000b2f12bebf32bc5006053a46bd9b226764
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-04-30 10:45:58 -0400

    changes to batch

--- backend/server.bat
+++ backend/server.bat
@@ -1,17 +1,15 @@
-setlocal
-Set MONGODB_HOSTNAME="localhost"
-Set MONGODB_DATABASE="omnidb"
-Set MONGODB_USERNAME=""
-Set MONGODB_PASSWORD=""
-Set FLASK_ENV="development"
-Set JWT_SECRET_KEY="omni-street-jwt-secret-key"
-Set MODEL_PATH="Models/"
-Set AI_API_KEY="c0opefv48v6rduk5oegg"
-Set API_KEY="mB9dUbVRc7Qq7rvvGX5bbBsuTuq2f9Y6"
-Set CELERY_BROKER_URL="redis://localhost:6379/0"
-Set CELERY_RESULT_BACKEND="redis://localhost:6379/0"
-Set RESET_TOKEN="3030207007026787"
-Set FLASK_DEBUG=1
-endlocal
+setx MONGODB_HOSTNAME "localhost"
+setx MONGODB_DATABASE "omnidb"
+setx MONGODB_USERNAME ""
+setx MONGODB_PASSWORD ""
+setx FLASK_ENV "development"
+setx JWT_SECRET_KEY "omni-street-jwt-secret-key"
+setx MODEL_PATH "Models/"
+setx AI_API_KEY "c0opefv48v6rduk5oegg"
+setx API_KEY "mB9dUbVRc7Qq7rvvGX5bbBsuTuq2f9Y6"
+setx CELERY_BROKER_URL "redis://localhost:6379/0"
+setx CELERY_RESULT_BACKEND "redis://localhost:6379/0"
+setx RESET_TOKEN "3030207007026787"
+setx FLASK_DEBUG 1
 
 waitress-serve --channel-timeout=300 --listen=*:5000 app:app
