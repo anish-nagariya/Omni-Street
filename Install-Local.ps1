be520baac2b19def8bd1f0ea8c2004d589c54f89
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-04-30 12:06:15 -0400

    changing script

--- Install-Local.ps1
+++ Install-Local.ps1
@@ -1,35 +1,24 @@
 Set-Location ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop))
 if (-Not (Test-Path .\omnistreet)) {
-    Write-Host 'Create Temp Path'
-    Set-Location ([System.Io.Path]::GetTempPath())
-    if (-Not (Test-Path  .\stockInstaller)) {
-        New-Item -Path .\stockInstaller -ItemType Directory
+    if (-Not (Test-Path  .\Installer)) {
+        New-Item -Path .\Installer -ItemType Directory
     }
-    Set-Location stockInstaller
+    Set-Location Installer
 
-    if (-Not(Test-Path .\mongodb-windows-x86_64-4.4.5-signed.msi) -or -Not(Test-Path .\node-v14.16.1-x64.msi) -or -Not(Test-Path .\python-3.8.9-amd64.exe) -or -Not(Test-Path .\Git-2.31.1-64-bit.exe)) {
+    if (-Not(Test-Path .\Git-2.31.1-64-bit.exe)) {
         Write-Host 'Downloading Installers'
-        Start-BitsTransfer -Source 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.5-signed.msi' -Destination '.\mongodb-windows-x86_64-4.4.5-signed.msi'
-        Start-BitsTransfer -Source 'https://nodejs.org/dist/v14.16.1/node-v14.16.1-x64.msi' -Destination '.\node-v14.16.1-x64.msi'
-        Start-BitsTransfer -Source 'https://www.python.org/ftp/python/3.8.9/python-3.8.9-amd64.exe' -Destination '.\python-3.8.9-amd64.exe'
         Start-BitsTransfer -Source 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe' -Destination '.\Git-2.31.1-64-bit.exe'
+        Start-BitsTransfer -Source 'https://desktop.docker.com/win/stable/amd64/Docker%20Desktop%20Installer.exe' -Destination '.\DockerDesktopInstaller.exe'
     }
 
-    Write-Host 'Installing MongoDB'
-    Start-Process -FilePath 'mongodb-windows-x86_64-4.4.5-signed.msi' -ArgumentList "/norestart", "/l*v mongo-installer.txt" -Wait
-    Write-Host 'Installing Node'
-    Start-Process -FilePath 'node-v14.16.1-x64.msi' -ArgumentList "/norestart", "/lv node-installer.txt", "/passive" -Wait
-    Write-Host 'Installing Python 3.8'
-    Start-Process -FilePath 'python-3.8.9-amd64.exe' -ArgumentList "/passive", "PrependPath=1" -Wait
+    
     Write-Host 'Installing Git'
     Start-Process -FilePath 'Git-2.31.1-64-bit.exe' -ArgumentList "/norestart", "/VERYSILENT", "/COMPONENTS=icons,icons\desktop,ext,ext\shellhere,ext\guihere,gitlfs,assoc,assoc_sh,autoupdate" -Wait
-
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
-    choco install memurai-developer -y
+    Write-Host 'Installing Docker'
+    Start-Process -FilePath 'DockerDesktopInstaller.exe'
 
     Set-Location ..
-    Remove-Item .\stockInstaller -Recurse
-
+    Remove-Item .\Installer -Recurse
 
     Set-Location ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop))
     git clone https://sivasunken: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@github.com/anish-nagariya/Omni-Street.git omnistreet 2>&1 | Write-Host
@@ -38,32 +27,16 @@ if (-Not (Test-Path .\omnistreet)) {
 
 if (Test-Path .\omnistreet) {
     Set-Location omnistreet
-
     git pull 2>&1 | Write-Host
-    Write-Host "Installing frontend"
-    Set-Location frontend
-    npm install
-    npm install -g @angular/cli@7.3.9
-    Write-Host "Installing backend"
-    Set-Location ..\backend
-    pip install --upgrade pip
-    pip install waitress
-    pip install -r requirements.txt
-    Set-Location ..
-    Start-Process cmd -ArgumentList "/k 'C:\Program Files\Memurai\memurai.exe'"
-    Set-Location backend
-    Start-Process cmd -ArgumentList "/k server.bat"
-    Start-Process cmd -ArgumentList "/k celery.bat"
-    Set-Location ..\frontend
-    Start-Process cmd -ArgumentList "/k web.bat"
+    Invoke-Expression 'docker-compose up --build -d'
 }
 
 Write-Host 'Script Complete'
 # SIG # Begin signature block
 # MIIFdgYJKoZIhvcNAQcCoIIFZzCCBWMCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUeQ70SGIHeK9IWo/d6IQRfl50
-# QwigggMOMIIDCjCCAfKgAwIBAgIQegrd3EhqlI9AODQyG4TfiDANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQURJ0vogyWOHupc4t6Py4SZS42
+# eSOgggMOMIIDCjCCAfKgAwIBAgIQegrd3EhqlI9AODQyG4TfiDANBgkqhkiG9w0B
 # AQUFADAdMRswGQYDVQQDDBJMb2NhbCBDb2RlIFNpZ25pbmcwHhcNMjEwNDMwMTE0
 # MzUzWhcNMjIwNDMwMTIwMzUzWjAdMRswGQYDVQQDDBJMb2NhbCBDb2RlIFNpZ25p
 # bmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDsCBaYejWmce/yypLJ
@@ -83,11 +56,11 @@ Write-Host 'Script Complete'
 # Y2FsIENvZGUgU2lnbmluZwIQegrd3EhqlI9AODQyG4TfiDAJBgUrDgMCGgUAoHgw
 # GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
 # NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
-# FgQUxmftwi9nvWJ6nvgQtlXS9kYvz3cwDQYJKoZIhvcNAQEBBQAEggEAWNDrafef
-# +vb904v1meTMJa9Iuq7kV+YNxzCCuEJ8a5sZb/VT5uLNToJgGb/kaKc+9AsxKYOy
-# zOz03l5HVINFenneRxvXkcu4WOintnlCeQ+ky6zxYxuvKWO2q+EP0K448LDAV/48
-# 0g0ZLw4pv4QYpngbF8q5igjS9aRjVb9tmpXdhwi9BNyZvkR/wgbApBEbHQeSI5OU
-# KZ120e+b++s41nOoyGaha5mk8LeRUmqJxf8fn8k/wP3qZTbLqY7vQe9pTwabdvNF
-# YZPOwvlAs1RqJxwflAtrdux3cymPRgYHMqRJ6ak/NlV9JF8V2J7uuZAcps+Tu62L
-# mWrEn8OltevxZQ==
+# FgQUVOzVt1YdAsuABA0LknCxoMv8bV8wDQYJKoZIhvcNAQEBBQAEggEArA1MpXTN
+# 47YkZlw8buPR4Af8F74D5GUqygyvW6VEVmzIhXvYNul1z2mKsvt60amDqNMXrqDu
+# DBWvCTLDEORd/jacG5KGsGOidLvDj+2HcTCuG4kskkI3EX9KhLDABhuO/tmPOlFo
+# hMLa9dr0PybMLxI40iBALY3pUnj9j8J2uehfMnWCN0IBVs5gcNcFhiK+3cfyD8TO
+# uvj5wfmbX+oqZWX78pzyE6gscXxlHisy6FLucc0yFXaxbIYiPpPklu7qe/1lrrFS
+# T0vbZ8TchLeteJi7GuNbpzOKW/9c1Elfmu6mr7Scqhq+RcT2Cz9Se6Qv4r+cnMuk
+# fe4bJ+OSJp3u3g==
 # SIG # End signature block
